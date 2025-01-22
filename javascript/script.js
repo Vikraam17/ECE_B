e8786d7605a44f9ef1466cc20f96685e2be01046
Vikraam17 <vikyvino1773@gmail.com>
2025-01-22 11:13:29 +0530

    js added

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+function SecondButtonClicked(){
+  document.getElementById('ex').innerHTML="Hello from external js"
+}
+
+console.log("Hello world");
---
+++ javascript/variable.js
@@ -0,0 +1,24 @@
+var a;
+a=10;
+console.log(typeof a)
+
+let b;
+console.log(typeof b)
+
+var d="hai"
+console.log(typeof d)
+
+const c=30;
+console.log(c-d)
+
+const name ={
+  fname:"raja",
+  lname:"k"
+}
+console.log(typeof name)
+
+var a =null;
+console.log(typeof a)
+
+var a=true;
+console.log(typeof a)
